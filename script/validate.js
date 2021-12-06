75117eb0acb1b7194648736e83090904dce1e060
Yuriy-pusev <yura.pusev@mail.ru>
2021-12-06 19:43:34 +0300

    проектная работа 6

--- script/validate.js
+++ script/validate.js
@@ -3,7 +3,7 @@ const inputData = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
+    inactiveButtonClass: 'popup__submit_disabled',
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__error_visible',
   };
